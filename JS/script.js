d209ade3a6ab79e728f16d63685c5eeae35e2a16
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-10-03 23:58:03 +0530

    Started JS from scratch...

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+alert("Hello World")
+
+console.log("Code is runing...")
+
+var a = prompt("Enter your number")
+var isTrue = prompt("Are your sure you want to leave this page?")
+
+console.log("Your number is " + a)
