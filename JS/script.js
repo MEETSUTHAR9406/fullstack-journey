83e8b9ae1fe4f9b642b912b5f9530287375a9e5c
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-10-03 23:59:54 +0530

    Minor change

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,12 @@ alert("Hello World")
 console.log("Code is runing...")
 
 var a = prompt("Enter your number")
-var isTrue = prompt("Are your sure you want to leave this page?")
+var isTrue = confirm("Are your sure you want to leave this page?")
+
+if(isTrue) {
+    console.log("You are leaving.")
+} else {
+    console.log("You are not leaving.")
+}
 
 console.log("Your number is " + a)
