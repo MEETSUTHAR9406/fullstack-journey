757b4082ff3e4875780980c53780df5f7a50fe7c
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-10-09 19:45:24 +0530

    Added some JS basic concepts

---
+++ JS/JS_Conditionals.js
@@ -0,0 +1,27 @@
+console.log("Hello! I am conditional tutorial");
+
+let age = 18;
+let grace = 2;
+
+// console.log(age)
+// console.log(grace)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+age = 0;
+age = 1;
+
+if (age == 18) {
+  console.log("You can drive.");
+} else if (age == 0) {
+  console.log("Are you kidding?");
+  console.log("You are of age " + age);
+} else if (age == 1) {
+  console.log("You are kidding again?");
+} else {
+  console.log("You cannot drive.");
+}
