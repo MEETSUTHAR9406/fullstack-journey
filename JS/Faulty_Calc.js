1819a97b7b4377b00db69fa3a1b4139aa263d23c
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-10-11 23:58:35 +0530

    A program for a Faulty Calculator

---
+++ JS/Faulty_Calc.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")({
+    sigint: true
+});
+
+let num1 = Number(prompt("Enter first number: "))
+let num2 = Number(prompt("Enter second number: "))
+
+console.log("You entered " + num1 + " and " + num2 + "\n")
+
+console.log("What operation you want to do?\n1. Addition.\n2. Subtraction.\n3. Multiplication.\n4. Division.")
+let choice = Number(prompt("Enter your choice (num): "))
+
+if (Math.random() < 0.1) {
+    if (choice == 1) console.log(num1 - num2)
+    else if (choice == 2) {
+        if (num2 == 0) {
+            console.log("Enter valid number")
+            process.exit(0)
+        }
+        console.log(num1 / num2)
+    } else if (choice == 3) console.log(num1 + num2)
+    else if (choice == 4) console.log(num1 ** num2)
+} else {
+    if (choice == 1) console.log(num1 + num2)
+    else if (choice == 2) console.log(num1 - num2)
+    else if (choice == 3) console.log(num1 * num2)
+    else if (choice == 4) {
+        if (num2 == 0) {
+            console.log("Enter valid number")
+            process.exit(0)
+        }
+        console.log(num1 / num2)
+    }
+}
