78e8254355de815f0b332a05671e2af7569983dd
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-12-15 18:21:57 +0530

    Started again! From the Hello World!

---
+++ JS/Try.js
@@ -0,0 +1,16 @@
+name = 'Meet Suthar'
+console.log(name)
+
+age = 24
+console.log(age)
+
+x = null;
+console.log(x)
+
+y = undefined
+console.log(y)
+
+bool = true
+console.log(bool)
+
+console.log(typeof bool)
